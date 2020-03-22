c14c419392ac7005aefb754623c70f8908f4c093
Christian Miller <syrex.portstein@gmail.com>
2020-03-22 18:30:39 +0100

    Intro

--- App.js
+++ App.js
@@ -1,184 +1,128 @@
-import React, {useEffect, useCallback, useContext, useState} from 'react';
-import {
-  createAppContainer,
-  createSwitchNavigator
-} from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-
+import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, View, Text, Image, I18nManager, AsyncStorage } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import RealApp from "./RealApp";
 
+I18nManager.forceRTL(false);
 
-import AccountScreen from "./src/Screens/AccountScreen";
-import SigninScreen from "./src/Screens/SigninScreen";
-import SignupScreen from "./src/Screens/SignupScreen";
-import Authhome from './src/Screens/AuthhomeScreen';
-import NotifiScreen from './src/Screens/NotifiScreen';
-import HomeScreen from './src/Screens/HomeScreen';
-import ResolveofAuthScreen from './src/Screens/ResolveofAuthScreen';
-import {Provider as AuthProvider} from './src/context/AuthContext';
-import { setNavigator } from './src/NavigationRef';
-import {Provider as LocationProvider} from './src/context/LocationConext';
-import { Provider as NotifiProvider } from './src/context/NotificationContext';
-
-
-/*import * as TaskManager from 'expo-task-manager';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-
-
-import IconWithBadge from './src/components/Tabbaricons';
-
-/*const NotificationIconWithBadge = props => {
-  // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
-  return <IconWithBadge {...props} badgeCount={3} />;
-};*/
-
-//import {registerFetchTask} from './src/Hooks/Backgroundupdate';
-
-//const INTERVAL_TASKS = 5000;
-//const LOCATION_TASK_NAME = "background-location-task";
-
-/*registerFetchTask('teste', () => {
-  console.log('Rodando em background');
-}, INTERVAL_TASKS);
-*/
-
-const getTabBarIcon = (navigation, focused, tintColor) => {
-  const { routeName } = navigation.state;
-  let IconComponent = Ionicons;
-  let iconName;
-  if (routeName === 'Home') {
-    iconName = `ios-home`;
-    // We want to add badges to home tab icon
-  } else if (routeName === 'Notification') {
-    iconName = `ios-notifications-outline`;
-    //IconComponent = NotificationIconWithBadge;
-  }else{
-    if(routeName === 'Account')
-    {
-      iconName = `md-person`;
-    }
-  }
-
-  // You can return any component that you like here!
-  return <Ionicons name={iconName} size={25} color={tintColor} />;
-};
-
-
-
-const switchNavigator = createSwitchNavigator({
- ResolveAuth : ResolveofAuthScreen,
-  loginFlow: createStackNavigator({
-    authhome: Authhome,
-    signup: SignupScreen,
-    signin: SigninScreen
-  }),
-  mainFlow: createBottomTabNavigator({
-    Home: HomeScreen,
-    Notification: NotifiScreen,
-    Account: AccountScreen
+const styles = StyleSheet.create({
+  mainContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
   },
-  {
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) =>
-        getTabBarIcon(navigation, focused, tintColor),
-    }),
-    tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    },
-  })
+  image: {
+    width: 320,
+    height: 320,
+  },
+  text: {
+    color: "white",
+    backgroundColor: "transparent",
+    textAlign: "center",
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 22,
+    color: "white",
+    backgroundColor: "transparent",
+    textAlign: "center",
+    marginBottom: 16,
+  },
 });
 
-const App = createAppContainer(switchNavigator);
+const slides = [
+  {
+    key: "somethun",
+    title: "Helpy finds your contact persons.",
+    text: "As long as Helpy is open, we will find contact persons.",
+    icon: "ios-search",
+    colors: ["#FF6366", "#FF6366"],
+  },
+  {
+    key: "somethun1",
+    title: "Are you infected?",
+    text: "If you are infected then hold the button and Helpy notifies your contact persons.",
+    icon: "ios-contacts",
+    colors: ["#EB4E68", "#EB4E68"],
+  },
+  {
+    key: "somethun2",
+    title: "Finished",
+    text: "You are ready to go.\n Remember your data is save and anonymous.",
+    icon: "ios-checkmark-circle-outline",
+    colors: ["#2F2E41", "#2F2E41"],
+  },
+];
 
-export default () => {
-
-  return(
-    <NotifiProvider>
-      <LocationProvider>
-        <AuthProvider>
-          <App ref={(navigator)=> {setNavigator(navigator)}} />
-        </AuthProvider>
-      </LocationProvider>
-    </NotifiProvider>
+export default class App extends React.Component {
+  // componentDidMount() {
+  //   AsyncStorage.getItem("first_item").then((value) => {
+  //     this.setState({ showRealApp: value });
+  //   });
+  // }
+  state = { showRealApp: false };
+  _renderItem = ({ item, dimensions }) => (
+    <LinearGradient
+      style={[
+        styles.mainContent,
+        {
+          flex: 1,
+          paddingTop: item.topSpacer,
+          paddingBottom: item.bottomSpacer,
+          width: dimensions.width,
+        },
+      ]}
+      colors={item.colors}
+      start={{ x: 0, y: 0.1 }}
+      end={{ x: 0.1, y: 1 }}
+    >
+      <Ionicons
+        style={{ backgroundColor: 'transparent' }}
+        name={item.icon}
+        size={200}
+        color="white"
+      />
+      <View>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
+    </LinearGradient>
   );
-};
-
-/*test for background location tracking*/
-/**************************************************************************** */
-  /*useEffect( () => {
-    _startTheTask();
-  }, []);
-
-  _startTheTask = async () =>{
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status === "granted") {
-      _getLocationAsync();
-    } else {
-      console.log('errorrrrr')
+  _onDone = () => {
+    // User finished the introduction. Show real app through
+    // navigation or simply by controlling state
+    // this.setState({ showRealApp: true });
+    AsyncStorage.setItem("first_item", "true").then(() => {
+      this.setState({ showRealApp: true });
+    });
+  }
+  _onSkip = () => {
+    // User finished the introduction. Show real app through
+    // navigation or simply by controlling state
+    // this.setState({ showRealApp: true });
+    AsyncStorage.setItem("first_item", "true").then(() => {
+      this.setState({ showRealApp: true });
+    });
+  }
+  render() {
+    if (this.state.showRealApp)
+    {
+      return <RealApp />;
+    }
+    else
+    {
+      return (
+        <AppIntroSlider
+          slides={slides}
+          renderItem={this._renderItem}
+          showPrevButton
+          showSkipButton
+          onDone={this._onDone}
+          onSkip={this._onSkip}
+        />
+      );
     }
   }
-  _getLocationAsync = async () => {
-    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-      enableHighAccuracy: true,
-      accuracy: Location.Accuracy.BestForNavigation,
-      distanceInterval: 1,
-      timeInterval: 5000
-    });
-    // watchPositionAsync Return Lat & Long on Position Change
-    location = await Location.watchPositionAsync(
-      {
-        accuracy: Location.Accuracy.BestForNavigation,
-        distanceInterval: 1,
-        timeInterval: 10000
-      },
-      newLocation => {
-        let { coords } = newLocation;
-        // console.log(coords);
-        let region = {
-          latitude: coords.latitude,
-          longitude: coords.longitude,
-          latitudeDelta: 0.045,
-          longitudeDelta: 0.045
-        };
-      },
-      error => console.log(error)
-    );
-    return location;
-  };
-
-  /*const startWatching = async () => {
-    const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
-    
-    const { scope } = permissions.location.ios
-    //const { status } = await Location.requestPermissionsAsync();
-    const permissionStatus = 1;
-    if (Platform.OS === 'ios') {
-      const { scope } = permissions.location.ios
-    } 
-    if (status === 'granted' && scope === 'always') {
-      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-        accuracy: Location.Accuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 1 
-      });
-     }
-  };*/
-  
-
-
-/*TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  if (data) {
-    const { locations } = data;
-    let lat = locations[0].coords.latitude;
-    let long = locations[0].coords.longitude;
-   
-    console.log("Received new locations for user = ",locations);
-  }
-});*/
+}
---
+++ RealApp.js
@@ -0,0 +1,183 @@
+import React, {useEffect, useCallback, useContext, useState} from 'react';
+import {
+  createAppContainer,
+  createSwitchNavigator
+} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import { Ionicons } from '@expo/vector-icons';
+
+
+import AccountScreen from "./src/Screens/AccountScreen";
+import SigninScreen from "./src/Screens/SigninScreen";
+import SignupScreen from "./src/Screens/SignupScreen";
+import Authhome from './src/Screens/AuthhomeScreen';
+import NotifiScreen from './src/Screens/NotifiScreen';
+import HomeScreen from './src/Screens/HomeScreen';
+import ResolveofAuthScreen from './src/Screens/ResolveofAuthScreen';
+import {Provider as AuthProvider} from './src/context/AuthContext';
+import { setNavigator } from './src/NavigationRef';
+import {Provider as LocationProvider} from './src/context/LocationConext';
+import { Provider as NotifiProvider } from './src/context/NotificationContext';
+
+/*import * as TaskManager from 'expo-task-manager';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+
+import IconWithBadge from './src/components/Tabbaricons';
+
+/*const NotificationIconWithBadge = props => {
+  // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+  return <IconWithBadge {...props} badgeCount={3} />;
+};*/
+
+//import {registerFetchTask} from './src/Hooks/Backgroundupdate';
+
+//const INTERVAL_TASKS = 5000;
+//const LOCATION_TASK_NAME = "background-location-task";
+
+/*registerFetchTask('teste', () => {
+  console.log('Rodando em background');
+}, INTERVAL_TASKS);
+*/
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === 'Home') {
+    iconName = `ios-home`;
+    // We want to add badges to home tab icon
+  } else if (routeName === 'Notification') {
+    iconName = `ios-notifications-outline`;
+    //IconComponent = NotificationIconWithBadge;
+  }else{
+    if(routeName === 'Account')
+    {
+      iconName = `md-person`;
+    }
+  }
+
+  // You can return any component that you like here!
+  return <Ionicons name={iconName} size={25} color={tintColor} />;
+};
+
+
+
+const switchNavigator = createSwitchNavigator({
+ ResolveAuth : ResolveofAuthScreen,
+  loginFlow: createStackNavigator({
+    authhome: Authhome,
+    signup: SignupScreen,
+    signin: SigninScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    Home: HomeScreen,
+    Notification: NotifiScreen,
+    Account: AccountScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) =>
+        getTabBarIcon(navigation, focused, tintColor),
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  })
+});
+
+const RealApp = createAppContainer(switchNavigator);
+
+export default () => {
+
+  return(
+    <NotifiProvider>
+      <LocationProvider>
+        <AuthProvider>
+          <RealApp ref={(navigator)=> {setNavigator(navigator)}} />
+        </AuthProvider>
+      </LocationProvider>
+    </NotifiProvider>
+  );
+};
+
+/*test for background location tracking*/
+/**************************************************************************** */
+  /*useEffect( () => {
+    _startTheTask();
+  }, []);
+
+  _startTheTask = async () =>{
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === "granted") {
+      _getLocationAsync();
+    } else {
+      console.log('errorrrrr')
+    }
+  }
+  _getLocationAsync = async () => {
+    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      enableHighAccuracy: true,
+      accuracy: Location.Accuracy.BestForNavigation,
+      distanceInterval: 1,
+      timeInterval: 5000
+    });
+    // watchPositionAsync Return Lat & Long on Position Change
+    location = await Location.watchPositionAsync(
+      {
+        accuracy: Location.Accuracy.BestForNavigation,
+        distanceInterval: 1,
+        timeInterval: 10000
+      },
+      newLocation => {
+        let { coords } = newLocation;
+        // console.log(coords);
+        let region = {
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+          latitudeDelta: 0.045,
+          longitudeDelta: 0.045
+        };
+      },
+      error => console.log(error)
+    );
+    return location;
+  };
+
+  /*const startWatching = async () => {
+    const { status, permissions } = await Permissions.askAsync(Permissions.LOCATION);
+    
+    const { scope } = permissions.location.ios
+    //const { status } = await Location.requestPermissionsAsync();
+    const permissionStatus = 1;
+    if (Platform.OS === 'ios') {
+      const { scope } = permissions.location.ios
+    } 
+    if (status === 'granted' && scope === 'always') {
+      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+        accuracy: Location.Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 1 
+      });
+     }
+  };*/
+  
+
+
+/*TaskManager.defineTask(LOCATION_TASK_NAME, async ({ data, error }) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  if (data) {
+    const { locations } = data;
+    let lat = locations[0].coords.latitude;
+    let long = locations[0].coords.longitude;
+   
+    console.log("Received new locations for user = ",locations);
+  }
+});*/
