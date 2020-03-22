b6cf1106f84890a0d39eb187202020c5aa4c9ff1
Emil Hopf <emilhopf@hotmail.com>
2020-03-22 13:32:57 +0100

    'frontend'

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -6,7 +6,15 @@ function Donate()
 {
     return (
                 <Card elevation={20} cornerRadius={19} style={{ padding: 30, margin: 20 }}>
-                    <Text style={{ fontSize: 30, marginBottom: 10, textAlign: 'center', fontWeight: "bold" }}>Donate for Helpy</Text>
+                    <Text style={{ fontSize: 30, marginBottom: 10, textAlign: 'center', fontWeight: "bold" }}>Support Helpy</Text>
+
+            <Text style={{ fontSize: 20, textAlign: 'justify', paddingTop: 20 }}>
+                    We are a team of 3 students from Germany and would be grateful for any support.
+                     </Text>
+
+            <TouchableOpacity style={{ borderRadius: 19, padding: 20, marginTop: 20, backgroundColor: "#2F2E41", }} onPress={() => Linking.openURL('https://www.buymeacoffee.com/AliKarami')}>
+                <Text style={{ fontSize: 15, textAlign: 'center', fontWeight: "bold", color: 'white' }}>Buy us a coffee</Text>
+            </TouchableOpacity>
                 </Card>     
 
     )
