4b208e784bb79a38ceb7f75d4f1a2f49dc22b157
Emil Hopf <emilhopf@hotmail.com>
2020-03-23 20:53:06 +0100

    'updated'

--- src/Screens/SignupScreen.js
+++ src/Screens/SignupScreen.js
@@ -1,14 +1,14 @@
-import React, {useContext} from 'react';
-import {View, Text, StyleSheet, Image, SafeAreaView } from 'react-native';
-import {Context as Authcontext} from '../context/AuthContext';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, Image, SafeAreaView, Linking } from 'react-native';
+import { Context as Authcontext } from '../context/AuthContext';
 import { NavigationEvents } from 'react-navigation';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 
-const SignupScreen = ({ navigation, screenProps }) => { 
+const SignupScreen = ({ navigation, screenProps }) => {
 
   const { state, signup, clearErrorMessage } = useContext(Authcontext);
-  let { t, locale } = screenProps; 
+  let { t, locale } = screenProps;
   const signupbuttom = t('signup');
   const signuptext = t('siguptext');
   const already = t('alreadyhav');
@@ -16,28 +16,28 @@ const SignupScreen = ({ navigation, screenProps }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-  <View>
-      <NavigationEvents onWillBlur={clearErrorMessage} />
-    <AuthForm 
-    headerText={signuptext} 
-    errorMessage={state.errorMessage} 
-    onSubmit={signup}
-    buttontext={signupbuttom}
-    loadingvalue={false}
-    passwordtext={passwordtext}
-    />
-    <Text style={{marginHorizontal:15, marginBottom:15}}>Durch Klicken auf Registrierung stimmen Sie den Allgemeinen Geschäftsbedingungen zu</Text>
-   <NavLink 
-   textTitle={already}
-   navigatelin="signin"
-   />
-    </View>
+      <View>
+        <NavigationEvents onWillBlur={clearErrorMessage} />
+        <AuthForm
+          headerText={signuptext}
+          errorMessage={state.errorMessage}
+          onSubmit={signup}
+          buttontext={signupbuttom}
+          loadingvalue={false}
+          passwordtext={passwordtext}
+        />
+        <Text onPress={() => Linking.openURL('https://helpy-landing.now.sh/agb')} style={{ marginHorizontal: 15, marginBottom: 15 }}>Durch das Klicken auf Registrierung stimmen Sie den <Text style={{ color: "blue" }}>Allgemeinen Geschäftsbedingungen</Text> zu</Text>
+        <NavLink
+          textTitle={already}
+          navigatelin="signin"
+        />
+      </View>
     </SafeAreaView>
 
   );
 };
 
-SignupScreen.navigationOptions = ()=> {
+SignupScreen.navigationOptions = () => {
   return {
     headerShown: false,
   };
@@ -45,28 +45,28 @@ SignupScreen.navigationOptions = ()=> {
 
 const styles = StyleSheet.create({
 
-  container:{
-        flex:1,
-        justifyContent: 'center',
-        marginBottom:250,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
 
-    },
-    Image:{
-      marginTop:40,
-      height:400,
-      width:412,
-      right:1,
-      resizeMode: 'stretch',
-      position:'absolute',
-      top:310,
-      opacity: .8,
-      shadowColor: "black",
-      shadowOffset: { height: 2},
-      shadowOpacity: 0.9,
-    },
-    link:{
-      marginHorizontal:15,
-    }
+  },
+  Image: {
+    marginTop: 40,
+    height: 400,
+    width: 412,
+    right: 1,
+    resizeMode: 'stretch',
+    position: 'absolute',
+    top: 310,
+    opacity: .8,
+    shadowColor: "black",
+    shadowOffset: { height: 2 },
+    shadowOpacity: 0.9,
+  },
+  link: {
+    marginHorizontal: 15,
+  }
 
 });
 
