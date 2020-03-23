0829c476453864d6dd706a98d51081eb2b08e527
Ali Karami <kaali728@yahoo.com>
2020-03-23 20:20:17 +0100

    repair

---
+++ App.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { StyleSheet, View, Text, Image, I18nManager, AsyncStorage } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import RealApp from "./RealApp";
+
+I18nManager.forceRTL(false);
+
+
+const slides = [
+    {
+      key: "somethun",
+      title: "English: Helpy finds your contact persons.\nDeutsch: Helpy findet deine Konatpersonen.",
+      text: "As long as Helpy is open, we will find contact persons.\nDeutsch: Solange Helpy geöffnet ist finden wir deine Kontaktpersonen.",
+      icon: "ios-search",
+      colors: ["#FF6366", "#FF6366"],
+    },
+    {
+      key: "somethun1",
+      title: "English: Are you infected?\nDeutsch: Du bist infisziert?",
+      text: "If you are infected then hold the button and Helpy notifies your contact persons.\nDeutsch: Wenn du infisziert bist halte den Knopf gedrückt damit Helpy deine Konaktpersonen benachrichtigt.",
+      icon: "ios-contacts",
+      colors: ["#EB4E68", "#EB4E68"],
+    },
+    {
+      key: "somethun2",
+      title: "Finished\nDeutsch: Fertig",
+      text: "You are ready to go.\n Remember your data is save and anonymous.\nDeutsch: Du kannst die App nun benutzen. Denke daran, dass Helpy deine Daten sichern und Anonym speichert.",
+      icon: "ios-checkmark-circle-outline",
+      colors: ["#2F2E41", "#2F2E41"],
+    },
+  ];
+  
+  export default class App extends React.Component {
+    componentDidMount() {
+      AsyncStorage.getItem("first_item").then((value) => {
+        this.setState({ showRealApp: value });
+      });
+    }
+    state = { showRealApp: false };
+    _renderItem = ({ item, dimensions }) => (
+      <LinearGradient
+        style={[
+          styles.mainContent,
+          {
+            flex: 1,
+            paddingTop: item.topSpacer,
+            paddingBottom: item.bottomSpacer,
+            width: dimensions.width,
+          },
+        ]}
+        colors={item.colors}
+        start={{ x: 0, y: 0.1 }}
+        end={{ x: 0.1, y: 1 }}
+      >
+        <Ionicons
+          style={{ backgroundColor: 'transparent' }}
+          name={item.icon}
+          size={200}
+          color="white"
+        />
+        <View>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.text}>{item.text}</Text>
+        </View>
+      </LinearGradient>
+  
+    );
+    _onDone = () => {
+      // User finished the introduction. Show real app through
+      // navigation or simply by controlling state
+      // this.setState({ showRealApp: true });
+      AsyncStorage.setItem("first_item", "true").then(() => {
+        this.setState({ showRealApp: true });
+      });
+    }
+    _onSkip = () => {
+      // User finished the introduction. Show real app through
+      // navigation or simply by controlling state
+      // this.setState({ showRealApp: true });
+      AsyncStorage.setItem("first_item", "true").then(() => {
+        this.setState({ showRealApp: true });
+      });
+    }
+    render() {
+      if (this.state.showRealApp)
+      {
+        return <RealApp />;
+      }
+      else
+      {
+        return (
+          <AppIntroSlider
+            slides={slides}
+            renderItem={this._renderItem}
+            showPrevButton
+            showSkipButton
+            onDone={this._onDone}
+            onSkip={this._onSkip}
+          />
+        );
+      }
+    }
+  }
+
+
+const styles = StyleSheet.create({
+  mainContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  image: {
+    width: 320,
+    height: 320,
+  },
+  text: {
+    color: "white",
+    backgroundColor: "transparent",
+    textAlign: "center",
+    paddingHorizontal: 16,
+  },
+  title: {
+    fontSize: 22,
+    color: "white",
+    backgroundColor: "transparent",
+    textAlign: "center",
+    marginBottom: 16,
+  },
+});
