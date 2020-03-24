32c0a7237fe71b6c2dcf131d847784a4674d9475
Emil Hopf <emilhopf@hotmail.com>
2020-03-25 00:46:44 +0100

    added Video

--- helpy-landing/src/components/landing.js
+++ helpy-landing/src/components/landing.js
@@ -103,7 +103,7 @@ function Landing() {
                 <Player
                     playsInline
                     poster="/assets/poster.png"
-                    src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                    src="/video.mp4"
                 />
             </div> 
             : 
